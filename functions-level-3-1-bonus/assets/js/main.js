9be9e02d0ed692af75e2c9324625a85401e50c96
noctwo <mr.evok@gmail.com>
2024-04-18 15:20:55 +0200

    bonus done

---
+++ functions-level-3-1-bonus/assets/js/main.js
@@ -0,0 +1,29 @@
+function changeColorOne(){
+    let bgColor = document.querySelector("body");
+    let headlineColor = document.querySelector("h1")
+    bgColor.style.backgroundColor = " rgb(136,240,39)";
+    headlineColor.style.color = "rgba(147,0,255,1";
+}
+
+function changeColorTwo(){
+    let bgColor = document.querySelector("body");
+    let headlineColor = document.querySelector("h1")
+    bgColor.style.backgroundColor = " rgb(39,170,240)";
+    headlineColor.style.color = "rgba(255,216,0,1)";
+}
+
+function changeColorThree(){
+    let bgColor = document.querySelector("body");
+    let headlineColor = document.querySelector("h1")
+    bgColor.style.backgroundColor = " rgb(0,0,0)";
+    headlineColor.style.color = "rgba(255,147,0,1)";
+}
+
+function changeColorFour(){
+    let bgColor = document.querySelector("body");
+    let headlineColor = document.querySelector("h1")
+    bgColor.style.backgroundColor = " rgb(54,89,249)";
+    headlineColor.style.color = "rgba(0,255,246,1)";
+}
+
+
